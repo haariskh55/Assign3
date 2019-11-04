a1c362c7530b21e0bfebd2fe92726d8eb2d92efe
Haaris Khan <Haaris Khan@DESKTOP-TJJ4P9U>
2019-11-04 23:16:30 +0500

    patterns added

--- builder1/CD.java
+++
@@ -1,3 +0,0 @@
-public abstract class CD implements Packing{
-public abstract String pack();
-}//End of the CD class.
--- builder1/Company.java
+++
@@ -1,3 +0,0 @@
-public abstract class Company extends CD{
-   public abstract int price();
-}//End of the Company class.
